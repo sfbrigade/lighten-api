45a6726ff817e4af0398a65f2995a155be8c9b46
John Barbuto <john.barbuto@indiegogo.com>
2016-11-02 21:01:30 -0700

    Add custom smalluuid serializer field
    http://www.django-rest-framework.org/api-guide/fields/#custom-fields

--- lighten_api/serializers.py
+++ lighten_api/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from smalluuid import SmallUUID
 
 from .models import Organization
 
 
+class SmallUUIDField(serializers.Field):
+    def to_representation(self, obj):
+        return str(obj)
+
+    def to_internal_value(self, data):
+        return SmallUUID(data, version=1)
+
+
 class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
+    uuid = SmallUUIDField()
+
     class Meta:
         model = Organization
         fields = ('url', 'uuid', 'json',)
